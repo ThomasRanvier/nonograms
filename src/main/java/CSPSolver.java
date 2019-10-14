287c632fdef4e6510808720834cbf8e413f6c9eb
ThomasRanvier <ranvier.thomas.pro@gmail.com>
2019-10-14 20:06:20 +0200

    Add nonogram generation method

---
+++ src/main/java/CSPSolver.java
@@ -0,0 +1,85 @@
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.constraints.nary.automata.FA.FiniteAutomaton;
+import org.chocosolver.solver.constraints.nary.automata.FA.IAutomaton;
+import org.chocosolver.solver.variables.BoolVar;
+import org.chocosolver.util.tools.ArrayUtils;
+
+public class CSPSolver {
+    private static void regexpConstraints(BoolVar[] cells, int[] rest, Model model) {
+        StringBuilder regexp = new StringBuilder("0*");
+        int m = rest.length;
+        for (int i = 0; i < m; i++) {
+            regexp.append('1').append('{').append(rest[i]).append('}');
+            regexp.append('0');
+            regexp.append(i == m - 1 ? '*' : '+');
+        }
+        IAutomaton auto = new FiniteAutomaton(regexp.toString());
+        model.regular(cells, auto).post();
+    }
+
+    private static void constructiveConstraints(BoolVar[] cells, int[] seq, Model model) {
+        FiniteAutomaton auto = new FiniteAutomaton();
+        int si = auto.addState();
+        auto.setInitialState(si); // declare it as initial state
+        int i0 = auto.addState();
+        auto.addTransition(si, i0, 0); // first transition
+        auto.addTransition(i0, i0, 0); // second transition
+        int from = i0;
+        int m = seq.length;
+        for (int i = 0; i < m; i++) {
+            int ii = auto.addState();
+            // word can start with '1'
+            if(i == 0){
+                auto.addTransition(si, ii, 1);
+            }
+            auto.addTransition(from, ii, 1);
+            from = ii;
+            for(int j = 1; j < seq[i]; j++){
+                int jj = auto.addState();
+                auto.addTransition(from, jj, 1);
+                from = jj;
+            }
+            int ii0 = auto.addState();
+            auto.addTransition(from, ii0, 0);
+            auto.addTransition(ii0, ii0, 0);
+            // the word can end with '1' or '0'
+            if(i == m - 1){
+                auto.setFinal(from, ii0);
+            }
+            from = ii0;
+        }
+        model.regular(cells, auto).post();
+    }
+
+    public static void cspMethod(String method, int width, int height, int[][][] constraints) {
+        Model model = new Model("Nonogram");
+        BoolVar[][] cells = model.boolVarMatrix("c", height, width);
+        for (int i = 0; i < height; i++) {
+            if (method.equals("regexp")) {
+                regexpConstraints(cells[i], constraints[0][i], model);
+            } else {
+                constructiveConstraints(cells[i], constraints[0][i], model);
+            }
+        }
+        for (int j = 0; j < width; j++) {
+            if (method.equals("regexp")) {
+                regexpConstraints(ArrayUtils.getColumn(cells, j), constraints[1][j], model);
+            } else {
+                constructiveConstraints(ArrayUtils.getColumn(cells, j), constraints[1][j], model);
+            }
+        }
+        if(model.getSolver().solve()){
+            displayResult(cells);
+        }
+    }
+
+    private static void displayResult(BoolVar[][] cells) {
+        for (int i = 0; i < cells.length; i++) {
+            System.out.printf("\t");
+            for (int j = 0; j < cells[i].length; j++) {
+                System.out.printf(cells[i][j].getValue() == 1 ? "##" : "  ");
+            }
+            System.out.printf("\n");
+        }
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Utils {
+    public static int[][][] generateNonogram(int width, int height, double rate, int seed) {
+        boolean[][] nonogram = new boolean[width][height];
+        Random rand = seed <= 0 ? new Random() : new Random(seed);
+        int[][] linesConstraints = new int[height][];
+        int[][] columnsConstraints = new int[width][];
+        ArrayList<Integer>[] columnConstraints = new ArrayList[width];
+        for (int x = 0; x < width; x++) {
+            columnConstraints[x] = new ArrayList<Integer>();
+        }
+        int[] c = new int[width];
+        for (int y = 0; y < height; y++) {
+            String line = "";
+            ArrayList<Integer> lineConstraints = new ArrayList<Integer>();
+            int l = 0;
+            for (int x = 0; x < width; x++) {
+                nonogram[x][y] = rand.nextInt(100) < (rate * 100);
+                line += nonogram[x][y] ? "##" : "  ";
+                if (nonogram[x][y]) {
+                    l++;
+                    c[x]++;
+                    if (x == width - 1) {
+                        lineConstraints.add(l);
+                    }
+                    if (y == height - 1) {
+                        columnConstraints[x].add(c[x]);
+                    }
+                } else {
+                    if (l > 0) {
+                        lineConstraints.add(l);
+                    }
+                    if (c[x] > 0) {
+                        columnConstraints[x].add(c[x]);
+                    }
+                    l = 0;
+                    c[x] = 0;
+                }
+            }
+            int[] lineC = new int[lineConstraints.size()];
+            for (int i = 0; i < lineConstraints.size(); i++) {
+                lineC[i] = lineConstraints.get(i);
+            }
+            linesConstraints[y] = lineC;
+
+            for (int x = 0; x < width; x++) {
+                int[] columnC = new int[columnConstraints[x].size()];
+                for (int i = 0; i < columnConstraints[x].size(); i++) {
+                    columnC[i] = columnConstraints[x].get(i);
+                }
+                columnsConstraints[x] = columnC;
+            }
+            //System.out.println(line);
+        }
+
+        int[][][] constraints = new int[][][]{linesConstraints, columnsConstraints};
+        for (int y = 0; y < height; y++) {
+            String line = "";
+            for (int i = 0; i < constraints[0][y].length; i++) {
+                line += constraints[0][y][i] + " ";
+            }
+            //System.out.println(line);
+        }
+        //System.out.println();
+        for (int x = 0; x < width; x++) {
+            String line = "";
+            for (int i = 0; i < constraints[1][x].length; i++) {
+                line += constraints[1][x][i] + " ";
+            }
+            //System.out.println(line);
+        }
+        return constraints;
+    }
+}
